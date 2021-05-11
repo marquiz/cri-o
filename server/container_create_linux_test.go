032ad38adf9087dc11d6c1e984cc0bd4944e9bf5
Markus Lehtonen <markus.lehtonen@intel.com>
2021-05-11 10:06:23 +0300

    server: add unit test for rdtClassFromAnnotations()
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>

--- server/container_create_linux_test.go
+++ server/container_create_linux_test.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/cri-o/cri-o/pkg/annotations"
 	"github.com/cri-o/cri-o/server/cri/types"
 	"github.com/opencontainers/runtime-tools/generate"
 )
@@ -70,4 +71,36 @@ func TestAddOCIBindsForSys(t *testing.T) {
 	if howManySys != 1 {
 		t.Error("there is not a single /sys bind mount")
 	}
+}
+
+func TestRdtClassFromAnnotations(t *testing.T) {
+	containerName := "test-container"
+	containerAnnotations := map[string]string{annotations.RdtContainerAnnotation: "class-1"}
+	podAnnotations := map[string]string{
+		annotations.RdtPodAnnotationContainerPrefix + containerName: "class-2",
+		annotations.RdtPodAnnotation:                                "class-3"}
+
+	cls, _ := rdtClassFromAnnotations(containerName, containerAnnotations, podAnnotations)
+	if cls != "class-1" {
+		t.Errorf("invalid rdt class, expecting \"class-1\", got %q", cls)
+	}
+
+	delete(containerAnnotations, annotations.RdtContainerAnnotation)
+	cls, _ = rdtClassFromAnnotations(containerName, containerAnnotations, podAnnotations)
+	if cls != "class-2" {
+		t.Errorf("invalid rdt class, expecting \"class-2\", got %q", cls)
+	}
+
+	delete(podAnnotations, annotations.RdtPodAnnotationContainerPrefix+containerName)
+	cls, _ = rdtClassFromAnnotations(containerName, containerAnnotations, podAnnotations)
+	if cls != "class-3" {
+		t.Errorf("invalid rdt class, expecting \"class-3\", got %q", cls)
+	}
+
+	delete(podAnnotations, annotations.RdtPodAnnotation)
+	cls, ok := rdtClassFromAnnotations(containerName, containerAnnotations, podAnnotations)
+	if ok {
+		t.Errorf("unexpected rdt class for container: %q", cls)
+	}
+
 }
